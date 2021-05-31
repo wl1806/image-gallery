65db4aff6039ea52e7704153bdc08ddeb8199d39
William Lim <william@gorillaspace.co>
2021-05-31 17:25:16 +0700

    a commit after deadline - use babelrc

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
+// import React, { Fragment, useState, useEffect } from 'react';
 import '../styles/globals.css'
+// import "@babel/polyfill";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
